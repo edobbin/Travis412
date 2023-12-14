e78f9a26e56e7e86430502b4780141f651b8466a
edobbin <ethandobbin37@gmail.com>
2023-12-13 20:48:36 -0500

    dfdf

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -2,8 +2,7 @@ package travis_ci_tutorial_java;
 
 public class SimpleCalculator {
 	public int add(int a, int b) {
-		if(a ==1 && b==1)
-			return 0;
+	
 		return a + b;
 	}
 	public int minus(int a, int b) {
