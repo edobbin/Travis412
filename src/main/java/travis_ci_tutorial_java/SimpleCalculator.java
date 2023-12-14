44d303075127ee8be48749cc8c1550f2a69ddca3
Ethan Dobbin <75637438+edobbin@users.noreply.github.com>
2023-12-13 20:16:53 -0500

    Update SimpleCalculator.java

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -6,4 +6,15 @@ public class SimpleCalculator {
 			return 0;
 		return a + b;
 	}
+	public int minus(int a, int b) {
+		return a-b;
+	}
+	
+	public int divide(int a, int b) {
+		return a/b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a*b;
+	}
 }
